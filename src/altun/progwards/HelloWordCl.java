857a14c3237432c715dfead50f2a768811037c09
Сергей <sergeyaltundzhi@gmail.com>
2019-11-27 02:10:12 +0300

    Test Commit

---
+++ src/altun/progwards/HelloWordCl.java
@@ -0,0 +1,14 @@
+package altun.progwards;
+
+public class HelloWordCl {
+    static void helloWord(){
+        String str;
+        str = "Привет, мир!";
+        System.out.println(str);
+    }
+    public static void main(String[] args) {
+        helloWord();
+     //   HelloWordCl.helloWord();
+     //   altun.progwards.HelloWordCl.helloWord();
+    }
+}
